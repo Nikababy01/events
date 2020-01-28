93efa87215d3b95865531e15c777455ee9b319e1
Monique.Bass01 <monique.bass01@gmail.com>
2020-01-27 20:18:14 -0600

    changed card

---
+++ main.js
@@ -0,0 +1,130 @@
+const pies = [
+{
+    name: "Dutch Apple Pie",
+    price: 1000,
+    isWarm: false,
+    isOrganic: true,
+    crust: "extra flaky",
+    iceCream: "Vanilla Bean",
+    isAvailable: true,
+    imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJUNJM8mVcZFwaNR8VFIdR7zf21I2L_aGjssRPeSrJ11jotwGY&s>",
+    drinkPairing: "Milk",
+    instructor: "Zoe"
+},
+{
+    name: "Pi",
+    price: 3.14,
+    isWarm: false,
+    isOrganic: true,
+    crust: "Crumb",
+    iceCream: "Moose Tracks",
+    isAvailable: true,
+    imageUrl: "https://images.food52.com/AVIPwP_aqm9nkAvFQZJzBC240hQ=/1200x1200/a38d55e1-e09b-4758-b301-449967da4fee--buttermilk_chess_pie.jpg",
+    drinkPairing: "Milk",
+    instructor: "Mary"
+},
+{
+    name: "Chocolate",
+    price: 5.00,
+    isWarm: false,
+    isOrganic: true,
+    crust: "Crumb",
+    iceCream: "Strawberry",
+    isAvailable: true,
+    imageUrl: "https://spicysouthernkitchen.com/wp-content/uploads/chocolate-meringue-pie-20.jpg",
+    drinkPairing: "coffee",
+    instructor: "Monique"
+},
+{
+    name: "Chess",
+    price: 4.00,
+    isWarm: false,
+    isOrganic: true,
+    crust: "Gram",
+    iceCream: "Chocolate",
+    isAvailable: true,
+    imageUrl: "https://foodsogoodmall.com/wp-content/uploads/2016/11/Southern-Chess-Pie.jpg",
+    drinkPairing: "Water",
+    instructor: "Raymond"
+},
+{
+    name: "Strawberry",
+    price: 4.00,
+    isWarm: false,
+    isOrganic: true,
+    crust: "Gram",
+    iceCream: "Chocolate",
+    isAvailable: true,
+    imageUrl: "https://www.thecountrycook.net/wp-content/uploads/2019/04/slice-Shoneys-Strawberry-Pie.jpg",
+    drinkPairing: "Water",
+    instructor: "Chasity"   
+},
+{
+    name: "Peach Cobbler",
+    price: 4.00,
+    isWarm: false,
+    isOrganic: true,
+    crust: "Gram",
+    iceCream: "Vanilla",
+    isAvailable: true,
+    imageUrl: "https://www.thecountrycook.net/wp-content/uploads/2012/08/thumbnail.jpg",
+    drinkPairing: "Water",
+    instructor: "Jeff"
+},
+{
+    name: "Pecan",
+    price: 4.00,
+    isWarm: false,
+    isOrganic: true,
+    crust: "Gram",
+    iceCream: "Chocolate",
+    isAvailable: true,
+    imageUrl: "https://www.simplyrecipes.com/wp-content/uploads/2014/11/Pecan-Pie-LEAD-1.jpg",
+    drinkPairing: "Water",
+    instructor: "Kay"
+},
+{
+    name: "Lemon",
+    price: 4.00,
+    isWarm: false,
+    isOrganic: true,
+    crust: "Gram",
+    iceCream: "Chocolate",
+    isAvailable: true,
+    imageUrl: "https://www.kingarthurflour.com/sites/default/files/recipe_legacy/6645-3-large.jpg",
+    drinkPairing: "Water",
+    instructor: "Raymond"
+},
+];
+
+console.log('pies',pies);
+// 3 cards to a row
+// nice big images
+// display all information in the pie objects
+const printToDom= (divId,textToPrint) =>{
+    const selectedDiv=document.getElementById(divId);
+    selectedDiv.innerHTML=textToPrint; 
+};
+
+const pieBuilder = ()=> {
+let domString ='';
+for (let i = 0; i < pies.length; i++){
+    domString += `<div class ="piecards">`;
+    domString += `<h1 class="piename">${pies[i].name}</h1>`;
+    domString += `<img class="img" src="${pies[i].imageUrl}">`;
+    domString += `<p>Name:${pies[i].name}</p>`;
+    domString += `<p>price:${pies[i].price}</p>`;
+    domString += `<p>Warm:${pies[i].isWarm}</p>`;
+    domString += `<p>Organic:${pies[i].isOrganic}</p>`;
+    domString += `<p>Crust:${pies[i].crust}</p>`;
+    domString += `<p>Ice Cream:${pies[i].iceCream}</p>`;
+    domString += `<p>Available:${pies[i].isAvailable}</p>`;
+    domString += `<p>Drink:${pies[i].drinkPairing}</p>`;
+    domString += `<p>Instructor:${pies[i].instructor}</p>`
+    domString += `</div>`;
+}
+       
+    
+    printToDom('pies', domString)
+};
+pieBuilder();
